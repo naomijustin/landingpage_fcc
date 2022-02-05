56dc76541512e393472200afdfb5666d1a4a02af
Naomi Justin <naomi.z.justin@gmail.com>
2022-02-05 19:45:56 +1100

    Completed - all tests pass

--- script/script.js
+++ script/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
     
+    $('#submit').click(function(e) {
+        e.preventDefault();
+        $('#form input').removeClass('is-invalid');
+        var error = false;
 
+        var email = $('#email').val();
+        if (email == '' || email == null) {
+            error = true;
+            $('#form input').addClass('is-invalid');
+        }
+
+        if (error) {
+            return
+        } else if (!error) {
+            var rec = {
+                Email: email
+            }
+            $.ajax({
+                type: 'POST',
+                url: 'https://www.freecodecamp.com/email-submit',
+                data: rec,
+                success: console.log('success'),
+                dataType: 'json'
+            })
+        }
+
+    });
 });
